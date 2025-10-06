05225c49ae5fe9b162a1f0a03e74d1b26205443f
ulilabzr <absoruu@gmail.com>
2025-10-07 06:12:19 +0700

    managing folder

---
+++ src/NotesApp.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AddPage from "./pages/AddPage";
+import ArchivePage from "./pages/ArchivePage";
+import DetailPage from "./components/NotesDetail";
+import Navigation from "./components/Navigation";
+import NotFoundPage from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
+import { NotesProvider } from "./contexts/NotesContext";
+
+function NotesApp() {
+  return (
+    <NotesProvider>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/add" element={<AddPage />} />
+        <Route path="/arsip" element={<ArchivePage />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </NotesProvider>
+  );
+}
+
+export default NotesApp;
